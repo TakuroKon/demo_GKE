9dc6895a056dd1020157a41f51ccea97cd024e29
TakuroKon <takurourou@gmail.com>
2022-10-07 17:58:49 +0900

    修正

--- 02/main.go
+++ 02/main.go
@@ -20,9 +20,9 @@ func main() {
 }
 
 func hello(w http.ResponseWriter, r *http.Request) {
-	log.Printf("serving request: %s", r.URL.Path)
+	log.Printf("Server request: %s", r.URL.Path)
 	host, _ := os.Hostname()
-	fmt.Fprintf(w, "Hello. world!\n")
-	fmt.Fprintf(w, "Verstion: 1.0.0\n")
+	fmt.Fprintf(w, "Hello, world!\n")
+	fmt.Fprintf(w, "Version: 1.0.0\n")
 	fmt.Fprintf(w, "Hostname: %s\n", host)
 }
